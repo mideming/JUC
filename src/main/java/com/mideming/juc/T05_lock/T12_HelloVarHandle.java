a38782c052d7a6d3644b0631fec074b9c29e690b
mideming <873737055@qq.com>
2021-04-01 11:02:09 +0800

    varHandle

--- src/main/java/com/mideming/juc/T05_lock/T12_HelloVarHandle.java
+++ src/main/java/com/mideming/juc/T05_lock/T12_HelloVarHandle.java
@@ -1,39 +1,39 @@
-package com.mideming.juc.T05_lock;
-
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.VarHandle;
-
-/**
- * VarHandle也指向同一个引用，可以进行一些原子操作
- * 1.普通属性原子操作
- * 2.比反射快，直接操纵二进制码
- */
-public class T12_HelloVarHandle {
-    int x = 0;
-    private static VarHandle varHandle;
-    static {
-        try {
-            varHandle = MethodHandles.lookup().findVarHandle(T12_HelloVarHandle.class, "x", int.class);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        T12_HelloVarHandle t12 = new T12_HelloVarHandle();
-        System.out.println(varHandle.get(t12));
-        varHandle.set(t12,1);
-        System.out.println(t12.x);
-        System.out.println(varHandle.get(t12));
-
-        varHandle.compareAndSet(t12,1,2);
-        System.out.println(t12.x);
-        System.out.println(varHandle.get(t12));
-
-        varHandle.getAndAdd(t12,3);
-        System.out.println(t12.x);
-        System.out.println(varHandle.get(t12));
-    }
-}
+//package com.mideming.juc.T05_lock;
+//
+//import java.lang.invoke.MethodHandles;
+//import java.lang.invoke.VarHandle;
+//
+///**
+// * VarHandle也指向同一个引用，可以进行一些原子操作
+// * 1.普通属性原子操作
+// * 2.比反射快，直接操纵二进制码
+// */
+//public class T12_HelloVarHandle {
+//    int x = 0;
+//    private static VarHandle varHandle;
+//    static {
+//        try {
+//            varHandle = MethodHandles.lookup().findVarHandle(T12_HelloVarHandle.class, "x", int.class);
+//        } catch (NoSuchFieldException e) {
+//            e.printStackTrace();
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        T12_HelloVarHandle t12 = new T12_HelloVarHandle();
+//        System.out.println(varHandle.get(t12));
+//        varHandle.set(t12,1);
+//        System.out.println(t12.x);
+//        System.out.println(varHandle.get(t12));
+//
+//        varHandle.compareAndSet(t12,1,2);
+//        System.out.println(t12.x);
+//        System.out.println(varHandle.get(t12));
+//
+//        varHandle.getAndAdd(t12,3);
+//        System.out.println(t12.x);
+//        System.out.println(varHandle.get(t12));
+//    }
+//}
