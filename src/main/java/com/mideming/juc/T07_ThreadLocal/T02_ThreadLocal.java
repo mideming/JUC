425767a08862bfb1793161f998d9d8930f09252e
mideming <873737055@qq.com>
2021-04-01 09:39:01 +0800

    ThreadLocal

---
+++ src/main/java/com/mideming/juc/T07_ThreadLocal/T02_ThreadLocal.java
@@ -0,0 +1,32 @@
+package com.mideming.juc.T07_ThreadLocal;
+
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 变量线程独享
+ */
+public class T02_ThreadLocal {
+    static ThreadLocal<Integer> tl = new ThreadLocal<>();
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            tl.set(2);
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("线程2---" + tl.get());
+        }).start();
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            tl.set(1);
+            System.out.println("线程1---" + tl.get());
+        }).start();
+    }
+}
