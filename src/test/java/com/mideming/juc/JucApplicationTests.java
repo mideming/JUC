7502c715ab79151d1b30e3f27c25ecedb11cb8e3
mideming <873737055@qq.com>
2021-04-20 23:56:54 +0800

    test

--- src/test/java/com/mideming/juc/JucApplicationTests.java
+++ src/test/java/com/mideming/juc/JucApplicationTests.java
@@ -3,11 +3,39 @@ package com.mideming.juc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @SpringBootTest
 class JucApplicationTests {
 
     @Test
     void contextLoads() {
+        Long n = 1L;
+        Long b = 2L;
+        LocalDate localDate = LocalDate.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("YYYYMMdd");
+        String A = localDate.format(dateTimeFormatter);
+        long l = Long.parseLong(A) * n % b;
+        String format = String.format("%08d", l);
+        System.out.println(format);
+    }
+    private static final ThreadLocal<Map<Integer, List<Integer>>> issueCouponMap = ThreadLocal.withInitial(HashMap::new);
+    @Test
+    void test() {
+        for (int cardTemplateId = 11; cardTemplateId < 20; cardTemplateId++) {
+            for (int wid = 0; wid < 10; wid++) {
+                List<Integer> cardTemplateIds = issueCouponMap.get().computeIfAbsent(wid, k -> new ArrayList<>());
+                cardTemplateIds.add(cardTemplateId);
+            }
+        }
+        for (int wid = 0; wid < 10; wid++) {
+            System.out.println(issueCouponMap.get().get(wid));
+        }
     }
 
 }
